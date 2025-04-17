f1168f3feb20a4eab845ec8784d6c43d7bfc544d
gnherrera <ginolouisherrera@gmail.com>
2025-04-17 14:09:04 +0800

    table di pa maayos

--- frontend/src/components/ui/table/index.tsx
+++ frontend/src/components/ui/table/index.tsx
@@ -37,28 +37,44 @@ const Table: React.FC<TableProps> = ({ children, className }) => {
 };
 
 // TableHeader Component
+// const TableHeader: React.FC<TableHeaderProps> = ({ children, className }) => {
+//   return <thead className={className}>{children}</thead>;
+// };
 const TableHeader: React.FC<TableHeaderProps> = ({ children, className }) => {
-  return <thead className={className}>{children}</thead>;
+  return <thead className={className} style={{ backgroundColor: "#e0e0e0", fontSize: "16px" }}>{children}</thead>;
 };
-
 // TableBody Component
 const TableBody: React.FC<TableBodyProps> = ({ children, className }) => {
   return <tbody className={className}>{children}</tbody>;
 };
 
 // TableRow Component
+// const TableRow: React.FC<TableRowProps> = ({ children, className }) => {
+//   return <tr className={className}>{children}</tr>;
+// };
+
 const TableRow: React.FC<TableRowProps> = ({ children, className }) => {
-  return <tr className={className}>{children}</tr>;
+  return <tr className={className} style={{ fontSize: "16px" }}>{children}</tr>;
 };
 
 // TableCell Component
+// const TableCell: React.FC<TableCellProps> = ({
+//   children,
+//   isHeader = false,
+//   className,
+// }) => {
+//   const CellTag = isHeader ? "th" : "td";
+//   return <CellTag className={` ${className}`}>{children}</CellTag>;
+// };
+
+
 const TableCell: React.FC<TableCellProps> = ({
   children,
   isHeader = false,
   className,
 }) => {
   const CellTag = isHeader ? "th" : "td";
-  return <CellTag className={` ${className}`}>{children}</CellTag>;
+  return <CellTag className={` ${className}`} style={{ fontSize: "16px" }}>{children}</CellTag>;
 };
 
 export { Table, TableHeader, TableBody, TableRow, TableCell };
