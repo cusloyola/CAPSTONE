38df7fcb0024515af9e48ba9015184e44aafe60c
Gino Herrera <ginolouisherrera@gmail.com>
2025-07-16 12:09:03 +0800

    ui not done

--- frontend/src/roles/Admin/Project Management/ProjectInfo.jsx
+++ frontend/src/roles/Admin/Project Management/ProjectInfo.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import ProjectInfoCard from "./Project Info Modals/ProjectInfoCard.tsx";
+import ProjectInfoDetailedCard from "./Project Info Modals/ProjectInfoDetailedCard.tsx";
 
 const ProjectInfo = () => {
-    return(
-        <h1>Hello</h1>
+    // Import the ProjectInfoCard component
+
+    return (
+        <div className="space-y-6">
+            <ProjectInfoCard />
+            <ProjectInfoDetailedCard />
+        </div>
     );
 };
 
